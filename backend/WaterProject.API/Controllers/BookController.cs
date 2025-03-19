3096405fe564972567760bd825f009975024b7b5
prestonpark26 <153689632+prestonpark26@users.noreply.github.com>
2025-03-19 09:07:47 -0600

    Initial Commit

---
+++ backend/WaterProject.API/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11.API.Data;
+
+namespace Mission11.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _context;
+
+        public BookController(BookDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IEnumerable<Book> Get()
+        {
+            return _context.Books.ToList();
+        }
+    }
+}
